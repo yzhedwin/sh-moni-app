fbb45c7d0a83caa54335752f3ed94c67c1db3452
yzhedwin <zizo_edwin@hotmail.com>
2026-03-17 23:28:29 +0800

    add bottomsheet ux

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,12 +1,16 @@
 import Action from "@/components/action";
 import Transaction from "@/components/transactions";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { LinearGradient } from "expo-linear-gradient";
-import { router } from "expo-router";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useMemo, useRef } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function HomeScreen() {
+  const uploadSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
   return (
-    <ScrollView style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <LinearGradient
         colors={["#6ee7b7", "#34d399", "#059669"]}
         start={{ x: 0, y: 0 }}
@@ -24,16 +28,12 @@ export default function HomeScreen() {
           <Action
             icon="cloud-upload"
             label="Upload File"
-            onPress={() => {
-              router.push("/upload");
-            }}
+            onPress={() => uploadSheetRef.current?.expand()}
           />
           <Action
             icon="stats-chart"
             label="Analytics"
-            onPress={() => {
-              router.push("/analytics");
-            }}
+            // onPress={() => bottomSheetRef.current?.expand()}
           />
         </View>
       </View>
@@ -46,7 +46,35 @@ export default function HomeScreen() {
         <Transaction name="Salary" category="Income" amount="+ $2,400" />
         <Transaction name="Amazon" category="Shopping" amount="- $82.10" />
       </View>
-    </ScrollView>
+      <BottomSheet
+        ref={uploadSheetRef}
+        index={-1} // closed by default
+        snapPoints={snapPoints}
+        enablePanDownToClose={true}
+        handleIndicatorStyle={{
+          width: 40,
+          height: 5,
+          backgroundColor: "#ccc",
+          borderRadius: 10,
+          alignSelf: "center",
+          marginBottom: 10,
+        }}
+      >
+        <BottomSheetView style={{ padding: 20 }}>
+          <Text style={{ fontSize: 18, fontWeight: "600" }}>
+            Upload Options
+          </Text>
+
+          <Pressable style={styles.sheetButton}>
+            <Text>📷 Take Photo</Text>
+          </Pressable>
+
+          <Pressable style={styles.sheetButton}>
+            <Text>🖼️ Upload from Gallery</Text>
+          </Pressable>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 }
 const styles = StyleSheet.create({
@@ -86,5 +114,18 @@ const styles = StyleSheet.create({
   actionsRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  sheetButton: {
+    padding: 16,
+    marginTop: 12,
+    backgroundColor: "#f0fdf4",
+    borderRadius: 12,
   },
 });
